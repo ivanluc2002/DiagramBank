a7e6fc770ee01519d82ba708b38ebf7bb26cd7f9
Ivanluc2002 <i.lucchini@itsrizzoli.it>
2022-06-16 16:54:49 +0200

    feat&update : periodicità

--- src/main/java/org/example/Titolodistato.java
+++ src/main/java/org/example/Titolodistato.java
@@ -2,12 +2,12 @@ package org.example;
 
 import java.util.Date;
 
-public class Ttolodistato {
+public class Titolodistato {
     private String nome;
     private double costo;
     private Date scadenza;
 
-    public Ttolodistato(String nome, double costo, Date scadenza) {
+    public Titolodistato(String nome, double costo, Date scadenza) {
         this.nome = nome;
         this.costo = costo;
         this.scadenza = scadenza;
