7229aab44b715374f69afe771e6560b7fbc75818
Ivanluc2002 <i.lucchini@itsrizzoli.it>
2022-06-16 16:29:43 +0200

    feat&update : titolodistato

---
+++ src/main/java/org/example/Ttolodistato.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import java.util.Date;
+
+public class Ttolodistato {
+    private String nome;
+    private double costo;
+    private Date scadenza;
+
+    public Ttolodistato(String nome, double costo, Date scadenza) {
+        this.nome = nome;
+        this.costo = costo;
+        this.scadenza = scadenza;
+    }
+
+}
+
