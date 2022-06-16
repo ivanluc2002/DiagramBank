96858154783e1cb712c61b7f047e4462f546a852
Ivanluc2002 <i.lucchini@itsrizzoli.it>
2022-06-16 16:20:39 +0200

    Initial commit

---
+++ src/main/java/org/example/Cliente.java
@@ -0,0 +1,28 @@
+package org.example;
+
+public class Cliente {
+    private String domicilio;
+    private String nome;
+
+    public Cliente(String domicilio, String nome) {
+        this.domicilio = domicilio;
+        this.nome = nome;
+    }
+
+    public void deposito() {
+        return ;
+    }
+
+    public void prelievo() {
+        return ;
+    }
+
+    public void acquista() {
+        return ;
+    }
+
+    public void richiedemutuo() {
+        return;
+    }
+}
+
