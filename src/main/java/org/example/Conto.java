8f99ee68b3b7bc2b36896f409bf7c16ec5e67f9f
Ivanluc2002 <i.lucchini@itsrizzoli.it>
2022-06-16 16:26:04 +0200

    update: conto

--- src/main/java/org/example/Conto.java
+++ src/main/java/org/example/Conto.java
@@ -1,4 +1,18 @@
 package org.example;
 
 public class Conto {
+    private String numero_conto;
+    private int saldo;
+
+    public Conto(String numero_conto, int saldo) {
+        this.numero_conto = numero_conto;
+        this.saldo = saldo;
+    }
+    public void acquistatitolo(){
+        return;
+    }
+    public void mutuo(){
+        return;
+    }
 }
+
